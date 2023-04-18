d15d6046f2174675fb1fe670031e4b00e5344e69
MinJeong1221 <mjeong1998@naver.com>
2023-04-18 13:00:16 +0900

    레이아웃만들기

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className={styles.nav}>Nav
     </div>
+    <div className={styles.header}>Header
+    </div>
+    <div className={styles.listContainer}>ListContainer
+    </div>
+    <div className={styles.footer}>Footer
+    </div>
+    </>
   );
 }
 
