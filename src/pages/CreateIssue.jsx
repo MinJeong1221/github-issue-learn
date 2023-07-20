47ef15de10b8dc0610d5fa24e415a802706f4cbe
MinJeong1221 <mjeong1998@naver.com>
2023-07-20 22:00:01 +0900

    수정

--- src/pages/CreateIssue.jsx
+++ src/pages/CreateIssue.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from "react";
+import React, { useRef } from "react";
 import cx from "clsx";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
